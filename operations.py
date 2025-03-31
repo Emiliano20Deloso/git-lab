c9b59fdb2b20d1856800c50c745a1ec6b8192195
jpnarchi <142521385+jpnarchi@users.noreply.github.com>
2025-03-31 12:52:32 -0600

    Agregue los resultados de las operaciones de suma y resta

--- operations.py
+++ operations.py
@@ -2,7 +2,7 @@ a = int(input(“Give me the first number:”))
 b = int(input(“Give me the second number:”))
 
 # Display the addition
-
+print("The addition of the two numbers is: ", a+b)
 # Display the subtraction
-
+print("The substraction of the two numbers is: ", a-b)
 # Display multiplication

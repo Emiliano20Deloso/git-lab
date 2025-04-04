9b002f75753ff0614d9726b38a8e9fc9fe2752d7
Efren Chavez <efrenchv05@gmail.com>
2025-04-03 22:20:30 -0600

    Deleted the Coment of #Display the multiplication

--- operations.py
+++ operations.py
@@ -4,10 +4,8 @@ b = int(input(“Give me the second number:”))
 # Display the addition
 print("The addition of the two numbers is: ", a+b)
 # Display the subtraction
-<<<<<<< HEAD
+
 print("The substraction of the two numbers is: ", a-b)
-# Display multiplication
-=======
 
 
 >>>>>>> Exfrenn
